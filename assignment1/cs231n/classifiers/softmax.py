2825be01828ae3d4d00a68c1a40f475500479be3
ottersem <58806533+ottersem@users.noreply.github.com>
2025-07-07 15:53:26 +0900

    softmax.py, softmax.ipynb completedd

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -34,7 +34,33 @@ def softmax_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    num_train = X.shape[0] # 데이터 수 N
+    num_classes = W.shape[1] # 클래스 수 C
+
+    for i in range(num_train):
+      scores = np.dot(X[i], W) # 각 클래스에 대한 score 벡터 (1,C)
+
+      scores -= max(scores) # 정규화(수치 안정화)
+
+      exp_scores = np.exp(scores)
+      probability = exp_scores / sum(exp_scores) # 소프트맥스 확률 계산
+
+      correct = y[i]
+      loss += -np.log(probability[correct]) # 정답 클래스의 softmax확률에 로그를 취해 크로스 엔트로피 로스 계산
+
+      for j in range(num_classes): # 클래스 j에 대한 gradient 벡터 (D,)
+        if j == correct:
+          dW[:,j] += (probability[j] - 1) * X[i]
+        else:
+          dW[:,j] += probability[j] * X[i]
+
+    # Loss 갱신
+    loss /= num_train
+    loss += reg * np.sum(W*W)
+
+    # gradient 갱신
+    dW /= num_train
+    dW += 2 * reg * W
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -59,8 +85,22 @@ def softmax_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    scores = np.dot(X,W) # (N,C)
+    scores -= np.max(scores, axis=1, keepdims=True)
 
+    num_train = scores.shape[0]
+
+    probs = np.exp(scores) / np.sum(np.exp(scores), axis=1, keepdims=1) # (N,C)
+
+    correct_class_probs = probs[np.arange(num_train), y] # (N,)
+    loss = -np.sum(np.log(correct_class_probs))/num_train
+    loss += reg * np.sum(W * W)
+
+    probs[np.arange(num_train), y] -= 1 # dL/dz : (N,C)
+    dW = np.dot(np.transpose(X), probs) # (D,C)
+    dW /= num_train
+    dW += 2 * reg * W
+    
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     return loss, dW
